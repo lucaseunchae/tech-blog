437b2460063786558e370d174e4cd991e7520242
Eunchae Kim <kec9706@gmail.com>
2024-04-18 02:18:26 +0900

    prism 테마 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,10 +19,10 @@ module.exports = {
           css: {
             ':not(pre) > code.language-text': {
               padding: '0.2em 0.4em !important',
-              color: `${theme('colors.slate.950')} !important`,
-              background: `${theme('colors.orange.100')} !important`,
+              color: `#fa1c1c !important`,
+              background: `rgba(135,131,120,.15) !important`,
               fontSize: '0.9em',
-              fontWeight: '400',
+              fontWeight: '500',
             },
           },
         },
