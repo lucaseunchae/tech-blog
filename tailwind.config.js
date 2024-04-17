534364505248074710efc512286b32803f9055cf
Eunchae Kim <kec9706@gmail.com>
2024-04-18 01:52:01 +0900

    gatsby-types.d.ts 무한 변경되는 버그 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [`./src/**/*.{js,jsx,ts,tsx}`],
+  content: [
+    `./src/pages/**/*.{js,jsx,ts,tsx}`,
+    `./src/components/**/*.{js,jsx,ts,tsx}`,
+    `./src/templates/**/*.{js,jsx,ts,tsx}`,
+  ],
   theme: {
     extend: {
       fontFamily: {
