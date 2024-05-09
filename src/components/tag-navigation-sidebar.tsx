5811e48b29b991735c45bc80ca9f18039cdd1d6f
Eunchae Kim <kec9706@gmail.com>
2024-05-09 17:54:57 +0900

    Tag Navigation Sidebar의 Navigation Item 컴포넌트 word break all css설정 추가

--- src/components/tag-navigation-sidebar.tsx
+++ src/components/tag-navigation-sidebar.tsx
@@ -6,10 +6,10 @@ function TagNavigationItem({
   totalCount,
 }: Queries.TagMetaDataQuery['allMarkdownRemark']['group'][0]) {
   return (
-    <li key={tag} className='leading-7 text-sm'>
+    <li key={tag} className='text-sm'>
       <Link
         to={`/tags/${tag}`}
-        className='hover:text-indigo-700'
+        className='break-all hover:text-indigo-700'
         activeClassName='text-indigo-700 font-semibold'
         partiallyActive={true}
       >
@@ -31,7 +31,7 @@ export default function ({ className }: { className?: string }) {
           <h5 className='mb-4 text-lg font-semibold text-gray-700 pb-2 border-b-2 border-solid border-gray-500'>
             태그
           </h5>
-          <ul>
+          <ul className='flex flex-col gap-2'>
             {group.map(({ tag, totalCount }) => (
               <TagNavigationItem key={tag} tag={tag} totalCount={totalCount} />
             ))}
